72669eda40b872b820b94162dba8cf0d85e39f9d
Turgay HOPAL <turgay24.th@gmail.com>
2023-01-25 15:40:59 +0300

    Update Configs for Realkom

--- app/config.py
+++ app/config.py
@@ -10,5 +10,5 @@ class Config:
     DATABASE = 'QualityDB'
 
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + USER + ':' + PASSWORD + '@' + HOST + '/' + DATABASE + '?driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.0.so.1.1'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + USER + ':' + PASSWORD + '@' + HOST + '/' + DATABASE + '?driver=SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
